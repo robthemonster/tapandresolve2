6e63c292e005c3d0c1ece2364a0ef4f65b2a93f9
robthemonster <robthemonsterlevin@gmail.com>
2019-03-12 01:59:33 -0400

    commenting out donation until account approved

--- src/page_components/AboutPage.vue
+++ src/page_components/AboutPage.vue
@@ -37,12 +37,12 @@
                     <p>
                         Your feedback and suggestions help improve the site. Please feel free to reach out.
                     </p>
-                    <p>
-                        Tap and Resolve is a self funded passion project. If ( and <b>only if</b> ) you are satisfied my work, you may donate to help mitigate web hosting and development costs.
-                    </p>
-                <b-row class="justify-content-center">
-                    <a class="dbox-donation-button" href="https://donorbox.org/tap-and-resolve" style="background:#007bff url(https://d1iczxrky3cnb2.cloudfront.net/red_logo.png) no-repeat 37px center; color: #fff;text-decoration: none;font-family: Verdana,sans-serif;display: inline-block;font-size: 16px;padding: 15px 38px 15px 75px; -webkit-border-radius: 2px; -moz-border-radius: 2px; border-radius: 2px; box-shadow: 0 1px 0 0 #1f5a89; text-shadow: 0 1px rgba(0, 0, 0, 0.3);" >Donate</a>
-                </b-row>
+                    <!--<p>-->
+                        <!--Tap and Resolve is a self funded passion project. If ( and <b>only if</b> ) you are satisfied my work, you may donate to help mitigate web hosting and development costs.-->
+                    <!--</p>-->
+                <!--<b-row class="justify-content-center">-->
+                    <!--<a class="dbox-donation-button" href="https://donorbox.org/tap-and-resolve" style="background:#007bff url(https://d1iczxrky3cnb2.cloudfront.net/red_logo.png) no-repeat 37px center; color: #fff;text-decoration: none;font-family: Verdana,sans-serif;display: inline-block;font-size: 16px;padding: 15px 38px 15px 75px; -webkit-border-radius: 2px; -moz-border-radius: 2px; border-radius: 2px; box-shadow: 0 1px 0 0 #1f5a89; text-shadow: 0 1px rgba(0, 0, 0, 0.3);" >Donate</a>-->
+                <!--</b-row>-->
             </b-jumbotron>
         </b-container>
         <Footer></Footer>
