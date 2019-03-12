6bdaace3c4ad670887cd207c70b7d010f972fdd9
robthemonster <robthemonsterlevin@gmail.com>
2019-03-12 19:51:47 -0400

    lazy loading list images

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -21,7 +21,7 @@
                     </b-col>
                     <b-col cols="5" class="text-center">
                     <span class="item_image">
-                    <b-img fluid :src="card.image_uris.border_crop"></b-img>
+                    <b-img-lazy fluid :src="card.image_uris.border_crop"></b-img-lazy>
                     </span>
                     </b-col>
                 </b-row>
