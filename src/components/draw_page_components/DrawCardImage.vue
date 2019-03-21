e04fd8f36ed760b5f382a6e1acb53110af028e1b
robthemonster <robthemonsterlevin@gmail.com>
2019-03-21 03:03:53 -0400

    loading spinner cuz duh

--- src/components/draw_page_components/DrawCardImage.vue
+++ src/components/draw_page_components/DrawCardImage.vue
@@ -1,8 +1,9 @@
 <template>
-    <b-row >
+    <b-row>
         <b-col>
-            <div class="text-center">
-                <b-img fluid v-bind:src="image_uri" :alt="card_name"></b-img>
+            <div id="image_div" class="text-center d-flex justify-content-center">
+                <b-img v-if="!loading" fluid v-bind:src="loadedUrl" :alt="card_name"></b-img>
+                <b-spinner v-if="loading" class="my-auto"></b-spinner>
             </div>
         </b-col>
     </b-row>
@@ -11,10 +12,27 @@
 <script>
     export default {
         props: ['image_uri', 'card_name'],
-        name: "DrawCardImage"
+        data() {
+            return {loading: true, loadedUrl: ""}
+        },
+        name: "DrawCardImage",
+        watch:{
+            'image_uri' : function() {
+                const image = new Image();
+                this.loading = true;
+                const outerThis = this;
+                image.onload = function() {
+                    outerThis.loadedUrl = outerThis.image_uri;
+                    outerThis.loading = false;
+                };
+                image.src = this.image_uri;
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    #image_div {
+        height: 680px;
+    }
 </style>
