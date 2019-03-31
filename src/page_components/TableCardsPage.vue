887279d392ffe9e3990abbc5abe1069db3fbf2de
robthemonster <robthemonsterlevin@gmail.com>
2019-03-31 18:14:13 -0400

    paginating results on liked/disliked pages

--- src/page_components/TableCardsPage.vue
+++ src/page_components/TableCardsPage.vue
@@ -1,15 +1,20 @@
 <template>
     <div>
         <b-container>
-            <b-form-input
-                    :state="(filterString !== '' && filteredCards.length === 0) ? false : filterString !== '' ? true : null "
-                    @input="filterCards" placeholder="Search by name" size="lg"
-                    v-model="filterString" list="filtered_card_list"></b-form-input>
+            <div class="sticky-top">
+                <div class="bg-dark text-white text-center">Displaying {{cards.length}} of {{size}}</div>
+                <b-form-input
+                        :state="(filterString !== '' && cards.length === 0) ? false : filterString !== '' ? true : null "
+                        @input="fetchCards(0)" placeholder="Search by name" size="lg"
+                        v-model="filterString" list="filtered_card_list"></b-form-input>
+
+            </div>
             <datalist id="filtered_card_list">
-                <option v-for="card in filteredCards" :key="card.id">{{card.name}}</option>
+                <option v-for="card in cards" :key="card.id">{{card.name}}</option>
             </datalist>
-            <CardList :card_list="displayedCards" v-on:open_modal="open_modal"></CardList>
-            <infinite-loading ref="infiniteLoader" :identifier="JSON.stringify(filteredCards)" class="text-white"
+            <CardList :card_list="cards" v-on:open_modal="open_modal"></CardList>
+
+            <infinite-loading v-if="loaderEnabled" ref="infiniteLoader" :identifier="filterString" class="text-white"
                               @infinite="addPage">
                 <div slot="no-more">No more cards</div>
                 <div slot="no-results">No cards</div>
@@ -25,7 +30,6 @@
 
     const axios = require('axios');
 
-    let cards = [];
     export default {
         components: {
             CardList,
@@ -34,65 +38,71 @@
         data: function () {
             return {
                 cards: [],
+                size: 0,
+                autocomplete: [],
                 page: 0,
                 PAGE_SIZE: 15,
-                filteredCards: cards,
-                displayedCards: cards,
-                filterString: ""
+                filterString: "",
+                loaderEnabled: false
             };
         },
         methods: {
             open_modal(card) {
                 this.$emit('open_modal', card)
             },
-            filterCards() {
-                let filteredCards = [];
-                this.page = 0;
-                this.$refs.infiniteLoader.stateChanger.reset();
-                const filterString = this.filterString.toLowerCase();
-                this.cards.forEach(function (card) {
-                    if (card.name.toLowerCase().includes(filterString)) {
-                        filteredCards.push(card);
-                    }
-                });
-                this.filteredCards = filteredCards;
-                this.displayedCards = filteredCards.slice(0, this.PAGE_SIZE);
-            },
-            fetchCards() {
+            async fetchCards(page) {
+                if (page === 0) {
+                    this.loaderEnabled = false;
+                }
                 const outerThis = this;
-                this.$parent.getAccount().then(function (account) {
-                    const data = {userid: account.id, token: account.token};
-                    axios.post(API_URL + outerThis.apiPath, data, JSON_HEADER).then(function (response) {
-                        outerThis.cards = response.data;
-                        outerThis.filterCards();
+                await this.$parent.getAccount().then(async function (account) {
+                    const data = {
+                        userid: account.id,
+                        token: account.token,
+                        page: page,
+                        pageSize: outerThis.PAGE_SIZE,
+                        filterString: outerThis.filterString
+                    };
+                    await axios.post(API_URL + outerThis.apiPath, data, JSON_HEADER).then(function (response) {
+                        if (page === 0) {
+                            outerThis.cards = [];
+                            outerThis.loaderEnabled = true;
+                        }
+                        outerThis.cards.push(...response.data.cards);
+                        outerThis.autocomplete = response.data.autocomplete;
+                        outerThis.size = response.data.size;
+                        if (response.data.cards.length > 0) {
+                            outerThis.page = page;
+                        }
                     });
                 });
             },
-            addPage($state) {
-                if (this.page * this.PAGE_SIZE < this.filteredCards.length) {
-                    this.page++;
-                    this.displayedCards.push(... this.filteredCards.slice(this.page * this.PAGE_SIZE,
-                        Math.min(this.filteredCards.length, (this.page + 1) * this.PAGE_SIZE)));
-                    $state.loaded();
-                } else {
+            async addPage($state) {
+                const prevPage = this.page;
+                await this.fetchCards(prevPage + 1);
+                if (this.page === prevPage) {
                     $state.complete();
+                } else {
+                    $state.loaded();
                 }
             }
         },
         mounted() {
-            this.fetchCards();
+            this.fetchCards(0);
             let outerThis = this;
-            this.$root.$on('bv::modal::hidden', () => {
-                outerThis.fetchCards()
+            this.$root.$on('bv::modal::hidden',async () => {
+                await outerThis.fetchCards(0)
             });
         },
         props: ['loggedIn', 'apiPath'],
         watch: {
-            'loggedIn': function () {
-                this.fetchCards();
+            'loggedIn': async function () {
+                await this.fetchCards(0);
+                this.$refs.infiniteLoader.stateChanger.reset();
             },
-            'apiPath': function () {
-                this.fetchCards();
+            'apiPath': async function () {
+                await this.fetchCards(0);
+                this.$refs.infiniteLoader.stateChanger.reset();
             }
         }
     }
