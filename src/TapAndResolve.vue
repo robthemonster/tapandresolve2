7eb16c220795b9ee77734aac84c792e555cc0952
robthemonster <robthemonsterlevin@gmail.com>
2019-05-25 21:53:12 -0400

    buhbye

--- src/TapAndResolve.vue
+++ src/TapAndResolve.vue
@@ -1,25 +1,26 @@
 <template>
     <div id="app">
-        <NavBar v-on:logout="logout" v-on:login="login" :loggedIn="loggedIn"></NavBar>
-        <router-view :loggedIn="loggedIn"
-                     v-on:like_card="likeCard"
-                     v-on:dislike_card="dislikeCard"
-                     v-on:update_user_card_status="getUserCardStatus"
-                     v-on:update_card_from_modal="updateCardFromModal"
-                     v-on:open_modal="openModalForCard"
-                     :modalOpen="modalOpen"
-                     :drawCard="drawCard"
-                     v-on:update_card_from_draw_page="updateDrawCard"
-                     class="bg-dark py-3"></router-view>
-        <CardModal
-                :card="modalCard"
-                :loggedIn="loggedIn"
-                v-on:like_card="likeCard"
-                v-on:dislike_card="dislikeCard"
-                v-on:update_user_card_status="getUserCardStatus"
-                v-on:update_card_from_modal="updateCardFromModal"
-        ></CardModal>
-        <Footer></Footer>
+<!--        <NavBar v-on:logout="logout" v-on:login="login" :loggedIn="loggedIn"></NavBar>-->
+<!--        <router-view :loggedIn="loggedIn"-->
+<!--                     v-on:like_card="likeCard"-->
+<!--                     v-on:dislike_card="dislikeCard"-->
+<!--                     v-on:update_user_card_status="getUserCardStatus"-->
+<!--                     v-on:update_card_from_modal="updateCardFromModal"-->
+<!--                     v-on:open_modal="openModalForCard"-->
+<!--                     :modalOpen="modalOpen"-->
+<!--                     :drawCard="drawCard"-->
+<!--                     v-on:update_card_from_draw_page="updateDrawCard"-->
+<!--                     class="bg-dark py-3"></router-view>-->
+<!--        <CardModal-->
+<!--                :card="modalCard"-->
+<!--                :loggedIn="loggedIn"-->
+<!--                v-on:like_card="likeCard"-->
+<!--                v-on:dislike_card="dislikeCard"-->
+<!--                v-on:update_user_card_status="getUserCardStatus"-->
+<!--                v-on:update_card_from_modal="updateCardFromModal"-->
+<!--        ></CardModal>-->
+<!--        <Footer></Footer>-->
+        Tap and Resolve is no longer.
     </div>
 </template>
 
@@ -31,9 +32,9 @@
     import 'bootstrap/dist/css/bootstrap.css'
     import 'bootstrap-vue/dist/bootstrap-vue.css'
     import VueCookies from 'vue-cookies'
-    import NavBar from "./components/NavBar.vue"
-    import Footer from "./components/Footer.vue"
-    import CardModal from "./components/CardModal.vue"
+    // import NavBar from "./components/NavBar.vue"
+    // import Footer from "./components/Footer.vue"
+    // import CardModal from "./components/CardModal.vue"
     import VueAnalytics from 'vue-analytics'
     import Toasted from 'vue-toasted'
     import {API_URL, DEFAULT_USER_CARD_STATUS, JSON_HEADER} from "@/constants";
@@ -68,9 +69,9 @@
     export default {
         name: 'app',
         components: {
-            NavBar,
-            Footer,
-            CardModal
+            // NavBar,
+            // Footer,
+            // CardModal
         },
         data: function () {
             return {loggedIn: loggedIn, modalOpen: modalOpen, modalCard: card, drawCard: card};
